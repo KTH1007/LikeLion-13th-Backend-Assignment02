a3a4b50a686b4621c1d07d3b4f9e95c0a74a23fc
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-04-11 21:54:57 +0900

    2주차 과제

--- kimtaeheon/SpringPrac/src/main/java/com/likelion/kimteaheon/SpringPracApplication.java
+++ kimtaeheon/SpringPrac/src/main/java/com/likelion/kimteaheon/SpringPracApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KimteaheonApplication {
+public class SpringPracApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KimteaheonApplication.class, args);
+        SpringApplication.run(SpringPracApplication.class, args);
     }
 
 }
